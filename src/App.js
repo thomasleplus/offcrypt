331188bf35886be3ebe67ba088a15d3f93d2e43d
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-07-15 16:48:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo" alt="logo' />
         <a
-          className="App-link"
-          href="https://github.com/thomasleplus/offcrypt"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://github.com/thomasleplus/offcrypt'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Source code on GitHub
         </a>
