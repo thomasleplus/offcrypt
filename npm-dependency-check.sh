40b34ff09e7df67a660efc334227fd2e0e11f867
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-07-18 11:42:09 -0700

    Create npm-dependency-check.sh

---
+++ npm-dependency-check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2016-present Thomas Leplus
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && \pwd)
+
+cd "${SCRIPT_DIR}"
+
+\npm install npm-check-updates
+
+rc=0
+
+while read -r l; do
+  \echo "${l}"
+  if [[ "${l}" == *"→"* ]]; then
+	  rc=$((rc+1))
+  fi
+done <<< "$(\ncu)"
+
+exit ${rc}
